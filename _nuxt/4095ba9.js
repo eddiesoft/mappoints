81c706eb90e554cff600b71194fc274a90e32271
Eddie98 <eddie.morra1234@gmail.com>
2023-01-14 04:19:24 +0600

    8ed3792

---
+++ _nuxt/4095ba9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{552:function(t,n,e){"use strict";e.r(n);var r=e(305),l=e(510),o={layout:"introduction"},c=e(63),component=Object(c.a)(o,(function(){var t=this._self._c;return t(l.a,[t(r.a,{attrs:{href:"/map",large:"",text:""}},[this._v("Перейти на карту")])],1)}),[],!1,null,null,null);n.default=component.exports}}]);
